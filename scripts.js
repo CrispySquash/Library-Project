76581ea90f9cfb0fc3b2773c3a89fc2e157811bb
Trevor Schaefer <shwow12@gmail.com>
2021-06-12 19:43:08 -0500

    Basically done with the moda popup for entering in a new book. just need to take data entered into form and add it to the array and display it on the page

--- scripts.js
+++ scripts.js
@@ -11,14 +11,37 @@ function addBookToLibrary() {
 
 }
 
-// Get the modal
-var modal = document.getElementById("modal-container");
+document.getElementById('modal-inputs').onsubmit = function (e) {
+    var titleValue = document.forms["register-book"]["title"].value;
+    var titleColor = document.forms["register-book"]["title"].style;
+    var authorValue = document.forms["register-book"]["author"].value;
+    var authorColor = document.forms["register-book"]["author"].style;
+    var pagesValue = document.forms["register-book"]["pages"].value;
+    var pagesColor = document.forms["register-book"]["pages"].style;
+    
+    if (titleValue == "" || titleValue == null) {
+        titleColor.backgroundColor = "rgba(255, 0, 0, 0.41)";
+    } else { titleColor.backgroundColor = "" }
 
-// Get the button that opens the modal
-var btn = document.getElementById("btn");
+    if (authorValue == "" || authorValue == null) {
+        authorColor.backgroundColor = "rgba(255, 0, 0, 0.41)";
+    } else { authorColor.backgroundColor = "" }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    if (pagesValue == "" || pagesValue == null) {
+        pagesColor.backgroundColor = "rgba(255, 0, 0, 0.41)";
+    } else { pagesColor.backgroundColor = "" }
+
+    e.preventDefault();
+    return false;
+}
+
+let submit = document.getElementById("submit-btn")
+let title = document.getElementById("title")
+let author = document.getElementById("author")
+let pages = document.getElementById("pages")
+let modal = document.getElementById("modal-container");
+let btn = document.getElementById("btn");
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
