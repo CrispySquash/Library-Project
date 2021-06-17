fcae333f201a0c2593c4141f4ee0ca566e634799
CrispySquash <50679388+CrispySquash@users.noreply.github.com>
2021-06-17 18:14:18 -0500

    a lot of work done here. book cards are now populated in and have correct user added information on them. also, i've added local storage so books are kept when revisiting the site. need to add a delete button on the books so users can remove them, and to figure out how to remove them from local storage as well

--- scripts.js
+++ scripts.js
@@ -1,41 +1,7 @@
-let myLibrary = [];
-
-function Book(author, title, numPages, haveRead) {
-    this.author = author;
-    this.title = title;
-    this.numPages = numPages;
-    this.haveRead = haveRead;
-}
-
-function addBookToLibrary() {
-
-}
-
-document.getElementById('modal-inputs').onsubmit = function (e) {
-    var titleValue = document.forms["register-book"]["title"].value;
-    var titleColor = document.forms["register-book"]["title"].style;
-    var authorValue = document.forms["register-book"]["author"].value;
-    var authorColor = document.forms["register-book"]["author"].style;
-    var pagesValue = document.forms["register-book"]["pages"].value;
-    var pagesColor = document.forms["register-book"]["pages"].style;
-    
-    if (titleValue == "" || titleValue == null) {
-        titleColor.backgroundColor = "rgba(255, 0, 0, 0.41)";
-    } else { titleColor.backgroundColor = "" }
-
-    if (authorValue == "" || authorValue == null) {
-        authorColor.backgroundColor = "rgba(255, 0, 0, 0.41)";
-    } else { authorColor.backgroundColor = "" }
-
-    if (pagesValue == "" || pagesValue == null) {
-        pagesColor.backgroundColor = "rgba(255, 0, 0, 0.41)";
-    } else { pagesColor.backgroundColor = "" }
-
-    e.preventDefault();
-    return false;
-}
-
+//event listener that runs function to add a new book once submit button is pressed
 let submit = document.getElementById("submit-btn")
+submit.addEventListener('click', addBookToLibrary);
+
 let title = document.getElementById("title")
 let author = document.getElementById("author")
 let pages = document.getElementById("pages")
@@ -43,19 +9,140 @@ let modal = document.getElementById("modal-container");
 let btn = document.getElementById("btn");
 let span = document.getElementsByClassName("close")[0];
 
+let titleValue = document.forms["register-book"]["title"].value;
+let titleColor = document.forms["register-book"]["title"].style;
+let authorValue = document.forms["register-book"]["author"].value;
+let authorColor = document.forms["register-book"]["author"].style;
+let pagesValue = document.forms["register-book"]["pages"].value;
+let pagesColor = document.forms["register-book"]["pages"].style;
+let readValue = document.getElementById('read').checked;
+
+let myLibrary = [];
+
+let newBook;
+
+//book constructor
+function Book(title, author, pages, read) {
+    this.title = titleValue;
+    this.author = authorValue
+    this.pages = pagesValue + ' pg/s';
+    this.read = readValue;
+}
+
+// function that pushes book to array
+function addBookToLibrary(e) {
+    e.preventDefault();
+    validateInputs(e);
+
+    if (validateInputs()) {
+        readValue = document.getElementById('read').checked;
+        modal.style.display = "none";
+        newBook = new Book(title, author, pages, read);
+        myLibrary.push(newBook);
+        populateLibrary(newBook);
+    }
+    populateStorage();
+}
+
+//preventing form with empty fields from being submitted
+function validateInputs(e) {
+    titleValue = document.forms["register-book"]["title"].value;
+    titleColor = document.forms["register-book"]["title"].style;
+    authorValue = document.forms["register-book"]["author"].value;
+    authorColor = document.forms["register-book"]["author"].style;
+    pagesValue = document.forms["register-book"]["pages"].value;
+    pagesColor = document.forms["register-book"]["pages"].style;
+
+    if (titleValue == "" || titleValue == null) {
+        titleColor.backgroundColor = "rgba(255, 0, 0, 0.41)";
+    } else { titleColor.backgroundColor = ""; }
+
+    if (authorValue == "" || authorValue == null) {
+        authorColor.backgroundColor = "rgba(255, 0, 0, 0.41)";
+    } else { authorColor.backgroundColor = ""; }
+
+    if (pagesValue == "" || pagesValue == null) {
+        pagesColor.backgroundColor = "rgba(255, 0, 0, 0.41)";
+    } else { pagesColor.backgroundColor = ""; }
+
+    if (titleValue == "" || titleValue == null || authorValue == "" ||
+        authorValue == null || pagesValue == "" || pagesValue == null) {
+        return false;
+    } else return true;
+}
+
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+// When the user clicks on the close button, close the modal
+span.onclick = function (e) {
     modal.style.display = "none";
+    e.preventDefault();
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
+window.onclick = function (e) {
+    if (e.target == modal) {
         modal.style.display = "none";
     }
+}
+
+//iterates through array
+myLibrary.forEach(populateLibrary)
+
+// function that creates/styles books from array
+function populateLibrary(e) {
+    let library = document.getElementById('library');
+    let bookDiv = document.createElement('div');
+    let titleDiv = document.createElement('div');
+    let authorDiv = document.createElement('div');
+    let pagesDiv = document.createElement('div');
+    let readDiv = document.createElement('div');
+    let title = e.title;
+    let author = e.author;
+    let pages = e.pages;
+    let read = e.read;
+
+    bookDiv.classList = 'book';
+    bookDiv.style.height = '200px'
+    bookDiv.style.width = '160px'
+    bookDiv.style.outline = 'solid';
+    library.appendChild(bookDiv);
+    bookDiv.appendChild(titleDiv);
+    titleDiv.innerHTML = `Title: ${title}`;
+    bookDiv.appendChild(authorDiv);
+    authorDiv.innerHTML = `Author: ${author}`;
+    bookDiv.appendChild(pagesDiv);
+    pagesDiv.innerHTML = `Pages: ${pages}`;
+    bookDiv.appendChild(readDiv);
+
+    if (e.read === true) { readDiv.textContent = 'Read'; }
+    else { readDiv.textContent = 'Not Read'; }
+
+    readDiv.addEventListener("click", function () {
+        e.read = !e.read;
+
+        if (e.read === true) { readDiv.textContent = 'Read'; }
+        else { readDiv.textContent = 'Not Read'; }
+    });
+
+    console.log(read)
+}
+
+const populateStorage = () => {
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+}
+
+const getStorage = () => {
+    currentLibrary = JSON.parse(localStorage.getItem('library'));
+    myLibrary = currentLibrary;
+    myLibrary.forEach(populateLibrary);
+}
+
+if (!localStorage.getItem('library')) {
+    populateStorage();
+} else {
+    getStorage();
 }
